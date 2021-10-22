76dd050cc5ab63fc69bd612fca88eb77d605c0fc
Grant Hutchinson <gxhutchinson@gmail.com>
2021-10-21 22:25:03 -0600

    make it actually work

--- app.py
+++ app.py
@@ -5,11 +5,24 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def ip():  # put application's code here
-    if flask.request.remote_addr == '127.0.0.1':
+    if flask.request.environ.get('HTTP_X_REAL_IP') is not None:
         IP = flask.request.environ.get('HTTP_X_REAL_IP', flask.request.remote_addr)
     else:
         IP = flask.request.remote_addr
-    return IP
+    return f"""<title>Your IP is {IP}</title><h1
+            id="age"
+            style="
+                font-size: 32px;
+                font-size: 3vw;
+                height: 100%;
+                width: 100%;
+                display: flex;
+                position: fixed;
+                align-items: center;
+                justify-content: center;
+            "
+        >Your public IP is {IP}
+        </h1>"""
 
 
 if __name__ == '__main__':
