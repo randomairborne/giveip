ff8b98375fa20e4cc4e89abbff4ee45aae1b04c9
valkyrie_pilot <valk@randomairborne.dev>
2021-12-28 11:25:29 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-git pull
+git fetch
+git reset --hard origin/master
+chmod +x update.sh
 RUSTFLAGS="-Ctarget-cpu=native --emit=asm" cargo build --release
 sudo systemctl daemon-reload
 sudo systemctl restart ip.mcfix.org.service
