21d001b7731310fe8a6596779c7b54a25613b3b6
valkyrie_pilot <valk@randomairborne.dev>
2023-01-08 20:07:49 -0700

    rewrite to axum_server

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,51 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-use std::convert::Infallible;
-#[cfg(debug_assertions)]
-use std::time::Instant;
+
+use std::net::SocketAddr;
+
+use axum::{extract::ConnectInfo, http::HeaderMap};
+use axum_server::{tls_rustls::RustlsConfig, Handle};
 
 #[allow(clippy::unused_async)]
-async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let headers = req.headers();
-    let addr = headers.get("X-Real-IP").map_or_else(
-        || "(failed to get)",
+async fn home(
+    ConnectInfo(sock_addr): ConnectInfo<SocketAddr>,
+    headers: HeaderMap,
+) -> ([(&'static str, &'static str); 1], String) {
+    let real_ip = headers.get("X-Real-IP").map_or_else(
+        || sock_addr.ip().to_string(),
         |ip| {
             ip.to_str()
-                .unwrap_or("(invalid ip address string header set by proxy)")
+                .map_or_else(|_| sock_addr.ip().to_string(), std::string::ToString::to_string)
         },
     );
-    let pretty_addr = format!("{addr}\n");
-    match req.uri().path() {
-        "/raw" => return Ok(Response::new(Body::from(pretty_addr))),
-        "/robots.txt" => return Ok(Response::new(Body::from("User-Agent: *\nAllow: /"))),
-        _ => {}
-    }
     let accept = headers
         .get("Accept")
         .map_or("*/*", |x| x.to_str().unwrap_or("invalid header value"));
-    let body = if accept.contains("text/html") {
-        include_str!("index.html").to_string()
+    if accept.contains("text/html") {
+        (
+            [("Content-Type", "text/html; charset=utf-8")],
+            include_str!("index.html").to_string(),
+        )
     } else {
-        format!("{pretty_addr}\n")
-    };
-
-    Ok(Response::new(Body::from(body)))
+        (
+            [("Content-Type", "text/plain; charset=utf-8")],
+            format!("{real_ip}\n"),
+        )
+    }
+}
+#[allow(clippy::unused_async)]
+async fn raw(ConnectInfo(sock_addr): ConnectInfo<SocketAddr>, headers: HeaderMap) -> String {
+    let real_ip = headers.get("X-Real-IP").map_or_else(
+        || sock_addr.ip().to_string(),
+        |ip| {
+            ip.to_str()
+                .map_or_else(|_| sock_addr.ip().to_string(), std::string::ToString::to_string)
+        },
+    );
+    format!("{real_ip}\n")
 }
 
 #[tokio::main]
 async fn main() {
-    let make_service = make_service_fn(move |_| {
-        #[cfg(debug_assertions)]
-        let st = Instant::now();
-
-        let service = service_fn(handle);
-
-        let r = async move { Ok::<_, Infallible>(service) };
-
-        #[cfg(debug_assertions)]
-        {
-            let et = Instant::now();
-            let tt = et.duration_since(st);
-            println!("{}ns to handle request", tt.as_nanos());
-        }
-        r
-    });
-
     let addr = std::net::SocketAddr::from((
         [0, 0, 0, 0],
         std::env::var("PORT")
@@ -59,18 +53,49 @@ async fn main() {
             .parse::<u16>()
             .unwrap_or(8080),
     ));
-
-    let server = Server::bind(&addr)
-        .serve(make_service)
-        .with_graceful_shutdown(async {
-            tokio::signal::ctrl_c()
+    let tls_addr = std::net::SocketAddr::from((
+        [0, 0, 0, 0],
+        std::env::var("HTTPS_PORT")
+            .unwrap_or_else(|_| 4443.to_string())
+            .parse::<u16>()
+            .unwrap_or(4443),
+    ));
+    let app = axum::Router::new()
+        .route("/", axum::routing::get(home))
+        .route("/raw", axum::routing::get(raw))
+        .with_state(());
+    let handle = Handle::new();
+    let perhaps_cert = std::env::var("CERTIFICATE");
+    let perhaps_key = std::env::var("PRIVATE_KEY");
+    let sd_handle = handle.clone();
+    tokio::task::spawn(async move {
+        tokio::signal::ctrl_c().await.ok();
+        println!("Server shutting down...");
+        sd_handle.shutdown();
+    });
+    if perhaps_cert.is_ok() || perhaps_key.is_ok() {
+        if let (Ok(cert), Ok(key)) = (perhaps_cert, perhaps_key) {
+            let tls_cfg = RustlsConfig::from_pem(cert.into(), key.into())
                 .await
-                .expect("failed to listen for ctrl+c");
-        });
-
-    println!("Listening on http://{addr}");
-
-    if let Err(e) = server.await {
-        eprintln!("server error: {e}");
+                .expect("Error loading certificate or privkey!");
+            let app = app.clone();
+            let handle = handle.clone();
+            tokio::task::spawn(async move {
+                println!("Listening on https://{tls_addr}");
+                axum_server::bind_rustls(tls_addr, tls_cfg)
+                    .handle(handle)
+                    .serve(app.into_make_service_with_connect_info::<std::net::SocketAddr>())
+                    .await
+                    .expect("Failed to run https server");
+            });
+        } else {
+            panic!("Both CERTIFICATE and PRIVATE_KEY must be set to run https")
+        }
     }
+    println!("Listening on http://{addr}");
+    axum_server::bind(addr)
+        .handle(handle)
+        .serve(app.into_make_service_with_connect_info::<std::net::SocketAddr>())
+        .await
+        .expect("Failed to run http server");
 }
