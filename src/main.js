645443288db2be830c161b3e92b6254f8ed23047
valkyrie_pilot <valk@randomairborne.dev>
2024-03-03 01:37:34 -0700

    many improvements everywhere

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const root_dns_name = document.getElementById("root-dns-name").dataset.dnsName;
+const v4_endpoint = `https://v4.${root_dns_name}/raw`;
+const v6_endpoint = `https://v6.${root_dns_name}/raw`;
+const ip4 = document.getElementById("ip4");
+const ip6 = document.getElementById("ip6");
+const ip4txt = document.getElementById("ip4txt");
+const ip6txt = document.getElementById("ip6txt");
+if (ip4txt.hidden) {
+  fetch(v4_endpoint)
+    .then((req) => req.text())
+    .then((resp) => {
+      ip4.innerText = resp;
+      ip4txt.hidden = false;
+      document.getElementById("title").innerText = `Your IP is ${resp}`;
+    })
+    .catch((failed) => {
+      console.log("Request failed:" + failed);
+    });
+}
+if (ip6txt.hidden) {
+  fetch(v6_endpoint)
+    .then((req) => req.text())
+    .then((resp) => {
+      ip6.innerText = resp;
+      ip6txt.hidden = false;
+    })
+    .catch((failed) => {
+      console.log("Request failed:" + failed);
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
-use std::fmt::{Display, Formatter};
 use std::{
-    net::{IpAddr, SocketAddr},
+    fmt::{Display, Formatter},
+    net::{IpAddr, Ipv6Addr, SocketAddr, SocketAddrV6},
     str::FromStr,
     sync::Arc,
 };
 
-use axum::extract::FromRequestParts;
-use axum::http::header::{ACCESS_CONTROL_ALLOW_ORIGIN, CACHE_CONTROL};
-use axum::http::request::Parts;
 use axum::{
-    extract::{ConnectInfo, Request},
-    http::{HeaderMap, HeaderName, HeaderValue, StatusCode},
+    extract::{ConnectInfo, FromRequestParts, Request, State},
+    http::{
+        header::{ACCESS_CONTROL_ALLOW_ORIGIN, CACHE_CONTROL, CONTENT_TYPE},
+        request::Parts,
+        HeaderMap, HeaderName, HeaderValue, StatusCode,
+    },
     middleware::Next,
-    response::{IntoResponse, Response},
+    response::{Html, IntoResponse, Response},
     routing::{any, get},
     Router,
 };
+use tera::{Context, Tera};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -26,22 +28,25 @@ async fn main() {
         .unwrap_or_else(|_| 8080.to_string())
         .parse::<u16>()
         .unwrap_or(8080);
-    let client_ip_var = std::env::var("CLIENT_IP_HEADER").ok();
-    let v4_addr = SocketAddr::from(([0; 4], port));
-    let v6_addr = SocketAddr::from(([0; 16], port));
-    let state = AppState::new(client_ip_var);
+    let v6_addr = SocketAddr::V6(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0));
+
+    let state = AppState::new();
+    let statics = Router::new()
+        .route("/main.css", get(css))
+        .route("/main.js", get(js))
+        .layer(axum::middleware::from_fn(noindex));
     let app = Router::new()
         .route("/raw", any(raw))
         .layer(axum::middleware::from_fn(noindex))
         .route("/", get(home))
-        .layer(axum::middleware::from_fn(nocors))
+        .layer(axum::middleware::from_fn(nocors_nocache))
+        .merge(statics)
+        .fallback(not_found)
         .with_state(state.clone());
 
-    println!("Listening on http://{v4_addr} and http://{v6_addr} for ip requests");
-    let tcp4 = TcpListener::bind(v4_addr).await.unwrap();
+    println!("Listening on http://localhost:{port} and http://{v6_addr} for ip requests");
     let tcp6 = TcpListener::bind(v6_addr).await.unwrap();
-
-    tokio::join!(svc(tcp4, app.clone()), svc(tcp6, app));
+    svc(tcp6, app).await;
 }
 
 async fn svc(tcp: TcpListener, app: Router) {
@@ -50,15 +55,27 @@ async fn svc(tcp: TcpListener, app: Router) {
         .await
         .unwrap();
 }
+
 #[allow(clippy::unused_async)]
-async fn home(IpAddress(ip): IpAddress, headers: HeaderMap) -> Result<HtmlOrRaw, Error> {
+async fn home(
+    IpAddress(ip): IpAddress,
+    headers: HeaderMap,
+    State(state): State<AppState>,
+) -> Result<Result<Html<String>, String>, Error> {
     let accept = headers
         .get("Accept")
         .map_or("*/*", |x| x.to_str().unwrap_or("invalid header value"));
     if accept.contains("text/html") {
-        Ok(HtmlOrRaw::Html(include_str!("index.html")))
+        let mut context = Context::new();
+        context.insert("root_dns_name", state.root_dns_name.as_ref());
+        if ip.is_ipv4() {
+            context.insert("ipv4", &ip.to_string());
+        } else {
+            context.insert("ipv6", &ip.to_string());
+        }
+        Ok(Ok(Html(state.tera.render("index.hbs", &context)?)))
     } else {
-        Ok(HtmlOrRaw::Raw(format!("{ip}\n")))
+        Ok(Err(format!("{ip}\n")))
     }
 }
 
@@ -67,20 +84,47 @@ async fn raw(IpAddress(ip): IpAddress) -> Result<String, Error> {
     Ok(format!("{ip}\n"))
 }
 
-static CORS_STAR: HeaderValue = HeaderValue::from_static("*");
+#[allow(clippy::unused_async)]
+async fn css() -> ([(HeaderName, HeaderValue); 1], &'static str) {
+    static CSS_CONTENT_TYPE: HeaderValue = HeaderValue::from_static("text/css;charset=UTF-8");
+    (
+        [(CONTENT_TYPE, CSS_CONTENT_TYPE.clone())],
+        include_str!("main.css"),
+    )
+}
 
-async fn nocors(request: Request, next: Next) -> Response {
+#[allow(clippy::unused_async)]
+async fn js() -> ([(HeaderName, HeaderValue); 1], &'static str) {
+    static JS_CONTENT_TYPE: HeaderValue = HeaderValue::from_static("text/javascript;charset=UTF-8");
+    (
+        [(CONTENT_TYPE, JS_CONTENT_TYPE.clone())],
+        include_str!("main.js"),
+    )
+}
+
+#[allow(clippy::unused_async)]
+async fn not_found() -> Html<&'static str> {
+    Html(include_str!("404.html"))
+}
+
+async fn nocors_nocache(request: Request, next: Next) -> Response {
+    static CORS_STAR: HeaderValue = HeaderValue::from_static("*");
+    static CACHE_CONTROL_VALUE: HeaderValue = HeaderValue::from_static("no-store");
+
     let mut response = next.run(request).await;
     response
         .headers_mut()
         .insert(ACCESS_CONTROL_ALLOW_ORIGIN, CORS_STAR.clone());
     response
+        .headers_mut()
+        .insert(CACHE_CONTROL, CACHE_CONTROL_VALUE.clone());
+    response
 }
 
-static ROBOTS_NAME: HeaderName = HeaderName::from_static("x-robots-tag");
-static ROBOTS_VALUE: HeaderValue = HeaderValue::from_static("noindex");
-
 async fn noindex(req: Request, next: Next) -> Response {
+    static ROBOTS_NAME: HeaderName = HeaderName::from_static("x-robots-tag");
+    static ROBOTS_VALUE: HeaderValue = HeaderValue::from_static("noindex");
+
     let mut resp = next.run(req).await;
     resp.headers_mut()
         .insert(ROBOTS_NAME.clone(), ROBOTS_VALUE.clone());
@@ -90,6 +134,8 @@ async fn noindex(req: Request, next: Next) -> Response {
 #[derive(Clone)]
 pub struct AppState {
     header: Option<Arc<HeaderName>>,
+    tera: Arc<Tera>,
+    root_dns_name: Arc<str>,
 }
 
 impl AppState {
@@ -97,31 +143,26 @@ impl AppState {
     /// # Panics
     /// This function can panic when its hardcoded values are invalid
     /// or the passed `client_ip_name` is not a valid header name
-    pub fn new(client_ip_name: Option<String>) -> Self {
+    pub fn new() -> Self {
+        let client_ip = std::env::var("CLIENT_IP_HEADER").ok();
+        let root_dns_name: Arc<str> = std::env::var("ROOT_DNS_NAME")
+            .expect("Requires ROOT_DNS_NAME in environment")
+            .into();
+        let mut tera = Tera::default();
+        tera.autoescape_on(vec!["hbs", "html", "htm"]);
+        tera.add_raw_template("index.hbs", include_str!("index.hbs"))
+            .unwrap();
         Self {
-            header: client_ip_name.map(|v| Arc::new(HeaderName::try_from(v).unwrap())),
+            header: client_ip.map(|v| Arc::new(HeaderName::try_from(v).unwrap())),
+            tera: Arc::new(tera),
+            root_dns_name,
         }
     }
 }
 
-pub enum HtmlOrRaw {
-    Html(&'static str),
-    Raw(String),
-}
-
-static CACHE_CONTROL_VALUE: HeaderValue = HeaderValue::from_static("no-store");
-
-impl IntoResponse for HtmlOrRaw {
-    fn into_response(self) -> Response {
-        match self {
-            Self::Html(v) => axum::response::Html(v).into_response(),
-            Self::Raw(v) => {
-                let mut resp = v.into_response();
-                resp.headers_mut()
-                    .insert(CACHE_CONTROL, CACHE_CONTROL_VALUE.clone());
-                resp
-            }
-        }
+impl Default for AppState {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -137,6 +178,7 @@ impl Display for IpAddress {
 #[axum::async_trait]
 impl FromRequestParts<AppState> for IpAddress {
     type Rejection = Error;
+
     async fn from_request_parts(
         parts: &mut Parts,
         state: &AppState,
@@ -163,6 +205,8 @@ pub enum Error {
     NoHeader,
     #[error("Could not extract connection info")]
     ConnectInfo,
+    #[error("Templating error")]
+    Tera(#[from] tera::Error),
     #[error("Could not convert supplied header to string (this is a configuration issue)")]
     ToStr(#[from] axum::http::header::ToStrError),
     #[error("Could not convert supplied header to IP address (this is a configuration issue)")]
